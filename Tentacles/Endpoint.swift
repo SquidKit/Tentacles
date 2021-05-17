3aad2c2987763c367a60dcba209d2e90967ab4f4
Mike Leavy <mleavy@gmail.com>
2021-05-17 16:00:38 -0700

    remove , and : as disallowed characters in urlQueryParametersAllowed

--- Tentacles/Endpoint.swift
+++ Tentacles/Endpoint.swift
@@ -746,8 +746,8 @@ open class Endpoint: Equatable, Hashable {
 extension CharacterSet {
     static var urlQueryParametersAllowed: CharacterSet {
         /// Does not include "?" or "/" due to RFC 3986 - Section 3.4
-        let generalDelimitersToEncode = ":#[]@"
-        let subDelimitersToEncode = "!$&'()*+,;="
+        let generalDelimitersToEncode = "#[]@"
+        let subDelimitersToEncode = "!$&'()*+;="
         
         var allowedCharacterSet = CharacterSet.urlQueryAllowed
         allowedCharacterSet.remove(charactersIn: "\(generalDelimitersToEncode)\(subDelimitersToEncode)")
